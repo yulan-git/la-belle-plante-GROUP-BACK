f8d4ea9719b7bab423fca519193c9db8f68d6fc4
gaelle1701 <gaellecoue@gmail.com>
2022-03-02 10:51:25 +0100

    modify port

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db/data.json')
 const middlewares = jsonServer.defaults()
+const port = process.env.port || 80
 
 // ___________ SERVER ______________________
 server.use(middlewares)
 server.use(router)
-server.listen(4000, () => {
+server.listen(port, () => {
     console.log('JSON Server is run');
 })

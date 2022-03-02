e3be499a6f7a68d11caf4c80c1c013171556e905
gaelle1701 <gaellecoue@gmail.com>
2022-03-02 10:27:22 +0100

    add server

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
 // ___________ SERVER ______________________
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db/data.json')
 const middlewares = jsonServer.defaults()
-
-
-app.use(express.static(__dirname + '/dist/la-belle-plante'));
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/la-belle-plante/index.html') );
-});
-app.listen(process.env.PORT || 8080);
 
 // ___________ SERVER ______________________
 server.use(middlewares)
